dbcf7d12e7cde3daf107756de86010a64e69f952
bt-ShigetaYusuke <yusuke_shigeta@booklook.jp>
2024-12-16 13:48:16 +0900

    ドリンクメニュー変更

--- page-drink-menu.php
+++ page-drink-menu.php
@@ -15,13 +15,192 @@
     </ul>
   </nav>
 
-  <?php
-  if (have_posts()) :
-    while (have_posts()) : the_post();
-      the_content();
-    endwhile;
-  endif;
-  ?>
+  <section id="all-you-can-drink" class="allyoucandrink common__width">
+    <h3 class="allyoucandrink">飲み放題メニュー</h3>
+    <table class="allyoucandrink__table">
+      <tbody>
+        <tr>
+          <th>Umeshu</th>
+          <td>梅酒ロック、梅酒水割り、梅酒ソーダ割り</td>
+        </tr>
+        <tr>
+          <th>Sour</th>
+          <td>レモン、巨峰、パイン、ピーチ、アセロラ、カルピス、グレープフルーツ</td>
+        </tr>
+        <tr>
+          <th>Softdrink</th>
+          <td>ウーロン茶、緑茶、ジャスミン茶、コカコーラ、カルピス、オレンジジュース、グレープフルーツジュース、ジンジャーエール</td>
+        </tr>
+        <tr>
+          <th>Cocktail</th>
+          <td>カシス、カシスオレンジ、カシスグレープ、カシスウーロン、カシスソーダ、ウォッカ、スクリュードライバー、モスコミュール、ブルドッグ、ルシアンコーク、ブラッディメアリー、ジン、ジントニック、ジンバック、ジンリッキー、オレンジブロッサム、ジンライム、マンゴー、マンゴヤンオレンジ、トロピカルファジー、マリブ、マリブオレンジ、マリブコーク、ライチ、ライチオレンジ、チャイナブルー、チャイナチャイナ、カンパリ、カンパリオレンジ、カンパリグレープ、カンパリソーダ、ピーチ、ファジーネーブル、レゲェパンチ、ラム、キューバリブレ</td>
+        </tr>
+      </tbody>
+    </table>
+  </section>
+
+  <section id="special-menu" class="special__menu common__width">
+    <h3 class="special__menu__title">LEGEND SPECIAL MENU（有料）</h3>
+    <table class="table table-champagne">
+      <thead>
+        <tr>
+          <th>Champagne</th>
+          <th></th>
+          <th>Price</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Veuve Clicquot</td>
+          <td>イエロー</td>
+          <td>¥30,000</td>
+        </tr>
+        <tr>
+          <td></td>
+          <td>ホワイト</td>
+          <td>¥35,000</td>
+        </tr>
+        <tr>
+          <td>UN</td>
+          <td>BRUUT / SWEET-ROSE</td>
+          <td>¥20,000</td>
+        </tr>
+        <tr>
+          <td>Moët &amp; Chandon</td>
+          <td>白</td>
+          <td>¥25,000</td>
+        </tr>
+        <tr>
+          <td></td>
+          <td>ロゼ</td>
+          <td>¥35,000</td>
+        </tr>
+        <tr>
+          <td></td>
+          <td>ネクターロゼ</td>
+          <td>¥40,000</td>
+        </tr>
+        <tr>
+          <td>PERRIER JOUET</td>
+          <td>BELLE EPOQUE</td>
+          <td>¥100,000</td>
+        </tr>
+        <tr>
+          <td>Veuve Clicquot</td>
+          <td>BELLE EPOQUE ROSE</td>
+          <td>¥200,000</td>
+        </tr>
+        <tr>
+          <td>Sou.mei</td>
+          <td>Brut</td>
+          <td>¥150,000</td>
+        </tr>
+        <tr>
+          <td>Dom Pérignon</td>
+          <td>白</td>
+          <td>¥100,000</td>
+        </tr>
+        <tr>
+          <td></td>
+          <td>ピンク</td>
+          <td>¥200,000</td>
+        </tr>
+        <tr>
+          <td></td>
+          <td>P2</td>
+          <td>¥250,000</td>
+        </tr>
+        <tr>
+          <td>ANGEL</td>
+          <td>Brut Black</td>
+          <td>¥200,000</td>
+        </tr>
+        <tr>
+          <td></td>
+          <td>Brut Rose White</td>
+          <td>¥250,000</td>
+        </tr>
+        <tr>
+          <td>Armand de brignac</td>
+          <td>Brut(Gold)</td>
+          <td>¥250,000</td>
+        </tr>
+      </tbody>
+    </table>
+    <table class="table table-wine">
+      <tbody>
+        <tr>
+          <th>Wine</th>
+          <th></th>
+          <th>Price</th>
+        </tr>
+        <tr>
+          <td>VOGA</td>
+          <td>白</td>
+          <td>¥20,000</td>
+        </tr>
+        <tr>
+          <td>689</td>
+          <td>赤</td>
+          <td>¥20,000</td>
+        </tr>
+      </tbody>
+    </table>
+    <table class="table table-foods">
+      <tbody>
+        <tr>
+          <th>Foods</th>
+          <th>Price</th>
+        </tr>
+        <tr>
+          <td>ジャーキー</td>
+          <td>¥1,000</td>
+        </tr>
+        <tr>
+          <td>ミックスナッツ</td>
+          <td>¥1,000</td>
+        </tr>
+        <tr>
+          <td>スナック盛り</td>
+          <td>¥1,000</td>
+        </tr>
+        <tr>
+          <td>ポップコーン</td>
+          <td>¥1,000</td>
+        </tr>
+        <tr>
+          <td>チーズ＆サラミ</td>
+          <td>¥2,000</td>
+        </tr>
+      </tbody>
+    </table>
+    <table class="table table-girlsDrink">
+      <tbody>
+        <tr>
+          <th>Girls Drink</th>
+          <th>Price</th>
+        </tr>
+        <tr>
+          <td>サワー/カクテル各種</td>
+          <td>¥1,500</td>
+        </tr>
+        <tr>
+          <td>ノンアルコール サングリア</td>
+          <td>¥2,000</td>
+        </tr>
+        <tr>
+          <td>MOZARTカクテル</td>
+          <td>¥2,500</td>
+        </tr>
+        <tr>
+          <td>果実酒各種</td>
+          <td>¥2,500</td>
+        </tr>
+      </tbody>
+    </table>
+    <p>※外注メニューは他にもご用意がございますので、ご所望の場合はどうぞお気軽にお申し付けください。ただし、状況次第ではご用意ができない場合がございます。あらかじめご了承ください。</p>
+    <p>※店内の状況によって、フードのご提供にはお時間を頂戴する場合がございますので、あらかじめご了承ください。</p>
+  </section>
 
 </main>
 
